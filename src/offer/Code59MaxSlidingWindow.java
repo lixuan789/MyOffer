2ff08ca40be9d39d3a16b762c1bbf094b51154f9
lixuan789 <1255470835@qq.com>
2020-08-06 15:36:02 +0800

    commit

--- src/offer/Code59MaxSlidingWindow.java
+++ src/offer/Code59MaxSlidingWindow.java
@@ -1,9 +1,6 @@
 package offer;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
@@ -31,5 +28,43 @@ public class Code59MaxSlidingWindow {
             ans[i]=ret.get(i);
         }
         return ans;
+    }
+
+    /**
+     * 单调队列
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow1(int[] nums, int k) {
+        if (nums==null||nums.length==0){
+            return new int[0];
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        Deque<Integer> dequeue=new LinkedList<>();
+        for (int i=0;i<nums.length;i++){
+            while (!dequeue.isEmpty()&&nums[dequeue.peekLast()]<nums[i]){
+                dequeue.pollLast();
+            }
+            dequeue.addLast(i);
+            if (dequeue.peekFirst()+k<=i){
+                dequeue.pollFirst();
+            }
+            if (i+1>=k){
+                list.add(nums[dequeue.peekFirst()]);
+            }
+        }
+        int[] ans=new int[list.size()];
+        for (int i=0;i<ans.length;i++){
+            ans[i]=list.get(i);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Code59MaxSlidingWindow test = new Code59MaxSlidingWindow();
+        int[] nums={1,3,-1,-3,5,3,6,7};
+        int[] a = test.maxSlidingWindow1(nums, 3);
+        System.out.println(Arrays.toString(a));
     }
 }
